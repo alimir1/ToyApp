699481f0928399a4ffff6d66cb4ae6c6ac904b63
alimir1 <110abidi@gmail.com>
2017-04-25 20:14:49 +0000

    finished toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "Welcome to Abidi!"
+    render html: "My name is Ishraq Abidi!"
   end
 end
